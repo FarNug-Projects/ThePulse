548fc35dc5e8956d9cb906469a5832ef16edf712
Brian Nugent <b.nugent@outlook.com>
2014-11-23 23:08:32 -0500

    Implemented the ability for the service to catch if there isn't an artist based on the search, and a controllable gallery for the news articles.

--- js/EchonestManager.js
+++ js/EchonestManager.js
@@ -4,67 +4,124 @@
 	var ECHONEST_GET_ARTIST_URL = "http://developer.echonest.com/api/v4/artist/search?api_key=X49FXQLHTSR1RNIBC";
 	var ECHONEST_GET_ARTIST_NEWS_URL = "http://developer.echonest.com/api/v4/artist/news?api_key=X49FXQLHTSR1RNIBC";
 	var artist;
-	var allNews = [];
+	var news = [];
+	var noArtist;
+	var noNews;
+	var articles = [];
+	var index = 0;
 	var echonestLoaded = false;
-
+	
+	var next = document.querySelector("#next");
+	var previous = document.querySelector("#previous");
+	next.addEventListener("click", nextArticle);
+	previous.addEventListener("click", previousArticle);
+	
+	function nextArticle(){
+		index++;
+		if(index >= articles.length) {
+			index = 0;
+		}
+		displayArticle(index);		
+	}
+	
+	function previousArticle(){
+		index--;
+		if(index <= 0) {
+			index = (articles.length - 1);
+		}
+		displayArticle(index);		
+	}
+	
 	function echonestSearch(artist, articleRange){
-		// Builds the artist search url and the news searh url
+		// Resets the data from the last search.
+		articles = [];
+		// Builds the artist search url and the news search url.
 		var searchArtistURL = ECHONEST_GET_ARTIST_URL + "&name=" + artist;
 		var searchNewsURL = ECHONEST_GET_ARTIST_NEWS_URL + "&name=" + artist + "&results=" + articleRange + "&start=0";
 		
 		$.getJSON(searchArtistURL).done(function(data){echonestArtistSearchJSONLoaded(data);});
 		$.getJSON(searchNewsURL).done(function(data){
 			echonestNewsSearchJSONLoaded(data);
-			buildNewsContainer();
+			buildArticleArray();
+			displayArticle(index);
 		});
 
 	}
 
 	function echonestArtistSearchJSONLoaded(obj) {
-		if(obj.error){
-			document.querySelector("#news").innerHTML = "<b>No Results Found</b>";
-		} else {
+		if(obj.response.artists == undefined) {
+			document.querySelector("#news").innerHTML = "<p>No Artist Found</b>";
+			$("#news").fadeIn(1000);
+		} else if(obj.response.artists.length > 0) {
 			var allArtists = obj.response.artists;
 			artist = allArtists[0].name;
 		}
 	}
 	
 	function echonestNewsSearchJSONLoaded(obj) {
-		if(obj.error){
-			document.querySelector("#news").innerHTML = "<b>No Results Found</b>";
-		} else {
-			allNews = obj.response.news;
+		if(obj.response.news == undefined) {
+			document.querySelector("#news").innerHTML = "<p>No News Found</b>";
+			$("#news").fadeIn(1000);
+		} else if(obj.response.news.length > 0) {
+			news = obj.response.news;
 		}
 	}
 	
-	// Builds the news div line by line based on the data in the news array.
-	function buildNewsContainer() {
-		var html = "";
-		var line = "";
-		
-		for (var i=0; i < allNews.length; i++){
-			var title = allNews[i].name;
-			var date = allNews[i].date_found;
-			var summary = allNews[i].summary;
-			var url = allNews[i].url;
-			
-			line += "<article>";
-			line +=  "<h3>";
-			line += title;
-			line += "</h3>";
-			line += "<strong>";
-			line += date;
-			line += "</strong>";
-			line += "<p>";
-			line += summary;
-			line +=  "</p>";
-			line += "<a href=" + url + " target='_blank'>" + url + "</a>";
-			line += "</article>";
-		}
-		html += line;
-		
-		document.querySelector("#news").innerHTML = html;
+	function displayArticle() {
+		// Populates the news section with one of the articles.
+		document.querySelector("#news").innerHTML = articles[index];
 		$("#news").fadeIn(1000);
+	}
+	
+	// Builds the news div line by line based on the data in the news array.
+	function buildArticleArray() {
+		if(news.length > 0) {
+			// Build an article entry for each news item found.
+			for (var i=0; i < news.length; i++){
+				var article = "";
+				
+				var title = news[i].name;
+				var date = news[i].date_found;
+				var summary = news[i].summary;
+				var url = news[i].url;
+				
+				article += "<article>";
+				article +=  "<h3>";
+				article += title;
+				article += "</h3>";
+				article += "<strong>";
+				article += date;
+				article += "</strong>";
+				article += "<p>";
+				article += summary;
+				article +=  "</p>";
+				article += "<a href=" + url + " target='_blank'>" + url + "</a>";
+				article += "</article>";
+				
+				// Store the article entry that was just made into the array of articles.
+				articles[i] = article;
+			}
+			
+			// When the article array has been built, clear the news array so future searches aren't affected.
+			news = [];
+		} else {
+			// Clear the array from any previous searches.
+			while(articles.length > 0) {
+				articles.pop();
+			}
+			
+			// Reset the index because there are no longer any articles.
+			index = 0;
+			
+			// Let the user know there are no articles for the artist.
+			var article = "";
+			article += "<p>";
+			article += "No news found for searched artist.";
+			article += "</p>";
+			
+			// Store the article entry that was just made into the array of articles.
+			articles[0] = article;
+		}
 		
 		echonestLoaded = true;
 	}
